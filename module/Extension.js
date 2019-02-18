20c0026b0d58cbba440633634d116f8a041f5e63
Pham Ngoc Duy <phamngocduy98@gmail.com>
2019-02-18 22:42:09 +0700

    update

--- module/Extension.js
+++ module/Extension.js
@@ -2,7 +2,7 @@ module.exports = (userInstance, bot) => {
     const downloadAppCallback = async (payload, chat) => {
         const joinID = payload.sender.id;
         console.log(`${joinID} download app...`);
-        var currentUser = userInstance.getInstance(joinID);
+        // var currentUser = userInstance.getInstance(joinID);
         chat.say({
             cards: [
                 {
@@ -27,4 +27,28 @@ module.exports = (userInstance, bot) => {
 
     bot.hear(/^\/tải_app$/, downloadAppCallback);
     bot.on('postback:DOWNLOAD_APP', downloadAppCallback);
+
+    const setupCallback = async (payload, chat) => {
+        const joinID = payload.sender.id;
+        console.log(`${joinID} setup game...`);
+        // var currentUser = userInstance.getInstance(joinID);
+        var roomID = userInstance.getRoomID(joinID);
+        chat.sendGenericTemplate([{
+            "title": "Ma sói Setup",
+            "image_url": `https://sites.google.com/site/masoibot/home/MaSoiLogo.png`,
+            "subtitle": "Set-up vai trò bằng tay",
+            "buttons": [{
+                "type": "web_url",
+                "url": `https://phamngocduy98.github.io/masoibot/setup?roomID=${roomID}`,
+                "title": "Set-up ngay",
+                "webview_height_ratio": "compact",
+                "messenger_extensions": true,
+                "fallback_url": `https://phamngocduy98.github.io/masoibot/setup?roomID=${roomID}`,
+                "webview_share_button": "hide"
+            }]
+        }]);
+    }
+
+    bot.hear(/^\/setup$/, setupCallback);
+    bot.on('postback:SETUP', setupCallback);
 };
